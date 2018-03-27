8f2c367d0229dfa3a4a3cd89eab1f0e4ac6abc3f
jocelio <jocelio27@gmail.com>
2018-03-26 23:55:38 -0300

    next queue action method

--- actions/fila.js
+++ actions/fila.js
@@ -7,35 +7,32 @@ export const GET_FILA = 'GET_FILA'
 export const MOVE = 'MOVE'
 export const MOVE_HEAD = 'MOVE_HEAD'
 export const CHANGE_STATUS = 'CHANGE_STATUS'
+export const NEXT_QUEUE = 'NEXT_QUEUE'
 
 
 const axios = axiosInstance();
 
-export const getFila = () => {
+export const getFila = () => ({
+    type: GET_FILA,
+    payload: axios.get(`/fila`)
+})
 
-    return {
-        type: GET_FILA,
-        payload: axios.get(`/fila`)
-    }
-}
+export const move = ({id, positions}) => ({
+    type: MOVE,
+    payload: axios.post(`/fila/move-up/${id}/${positions}`)
+})
 
-export const move = ({id, positions}) => {
-    return {
-        type: MOVE,
-        payload: axios.post(`/fila/move-up/${id}/${positions}`)
-    }
-}
+export const changeStatus = id => ({
+    type: CHANGE_STATUS,
+    payload: axios.put(`/fila/change-status/${id}`)
+})
 
-export const changeStatus = id => {
-    return {
-        type: CHANGE_STATUS,
-        payload: axios.put(`/fila/change-status/${id}`)
-    }
-}
+export const moveHead = () => ({
+    type: MOVE_HEAD,
+    payload: axios.post(`/fila/head`)
+})
 
-export const moveHead = () => {
-    return {
-        type: MOVE_HEAD,
-        payload: axios.post(`/fila/head`)
-    }
-}
+export const nextQueue = days => ({
+    type: NEXT_QUEUE,
+    payload: axios.post(`/fila/next-queue/${days}`)
+})
